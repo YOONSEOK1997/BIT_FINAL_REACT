6ec219f234f8b3e5e92fc24f156d78d95603407c
magnum11111 <assure2019@naver.com>
2022-07-19 12:00:32 +0900

    ABOUT

---
+++ src/pages/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const about = () => {
+  return (
+    <div>
+      <h1>우리</h1>
+        <div className="about">
+          <h2>탈잉을 소개합니다.</h2>
+            < span style="color:rgba(212, 76, 71, 1);fill:rgba(212, 76, 71, 1)" 
+                 data-token-index="0" class="notion-enable-hover" 탈잉 span />
+
+            </div>
+        </div>    
+    </div>
+  );
+};
+
+export default about;
