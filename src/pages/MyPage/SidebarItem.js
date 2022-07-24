b249b1a63003d87350690be8dca0a970c2027d48
Jungha <kidultree@naver.com>
2022-07-24 15:27:41 +0900

    [수정] 사이드바

--- src/pages/MyPage/SidebarItem.js
+++ src/pages/MyPage/SidebarItem.js
@@ -13,14 +13,12 @@ function SidebarItem({ menu }) {
 export default SidebarItem;
 
 const SidebarMenu = styled.div`
-  color: #111;
   font-size: 20px;
   font-family: Noto Sans KR;
   font-weight: 500;
   width: 200px;
   height: 30px;
-  border: 1px solid gray;
   text-align: center;
-  margin-top: 10px;
+  margin-top: 20px;
   display: inline-block;
 `;
