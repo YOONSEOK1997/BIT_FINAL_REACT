48ed91533a39c2b3407d6a90fc319bc1e8d456ca
YOONSEOK1997 <yoonsuk0629@gmail.com>
2022-07-25 18:52:37 +0900

    [수정] 클래스 리뷰

--- src/pages/class/ClassReview.js
+++ src/pages/class/ClassReview.js
@@ -1,57 +1,116 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Rating from '@mui/material/Rating';
+import './ClassReview.css';
+import { textAlign } from '@mui/system';
+import StarRatings from 'react-star-ratings';
 
-const ClassReview = () => {
+const ClassReview = ({ class_num }) => {
+  const [data, setData] = useState([]);
   const getReviews = () => {
     axios.get().then(res => {});
   };
 
   useEffect(() => {
-    getReviews();
-  }, []);
+    console.log('c1c' + class_num);
+    ReviewList();
+  }, [class_num]);
+
+  let photoUrl = process.env.REACT_APP_SPRING_URL + 'save/';
+  let reviewListUrl =
+    process.env.REACT_APP_SPRING_URL + 'review/alllist?class_num=' + class_num;
+
+  const ReviewList = () => {
+    axios
+      .get(reviewListUrl, {
+        class_num,
+      })
+      .then(res => {
+        setData(res.data);
+        console.log(res.data);
+      });
+  };
 
   return (
-    <div>
-      <div className="class_reviewrow">
-        {/* 리뷰info */}
-        <div className="class_reviewerinfo">
-          <div class="tutor_image" style={{ float: 'left' }}></div>
-          <span className="class_reviewername">김정하</span>
-          <br />
-          <span className="class_reviewertime">2022-07-04 03:58</span>
-          <div className="class_review_star">★★★★</div>
-
-          <textarea
-            className="class_reviewcontent"
-            style={{ fontSize: '15px' }}
-            placeholder="리뷰를 작성해주세요"
-          />
-          <div className="class_reviewphoto"></div>
-          <div className="commentlike"> ♥27 ㅁ12</div>
-          {/* 댓글 */}
-          <div className="class_reviewcomment">
-            <div className="re_info">
-              <div class="tutor_image" style={{ float: 'left' }}></div>
-              <span className="class_reviewername">이경주</span>
-              <br />
-              <span className="class_reviewertime">2022-07-04 03:58</span>
-            </div>
-            <div className="class_commentbody" style={{ fontSize: '15px' }}>
-              하하하 저도 배우고싶네용
-            </div>
-            <div className="class_commentdell">
-              <button
-                style={{ float: 'right', marginTop: '5px' }}
-                className="btn4"
+    <div className="class_reviewbox">
+      <table
+        className="table table-borderless"
+        id="review_table"
+        style={{ width: '100%' }}
+      >
+        <thead>
+          <tr
+            style={{
+              backgroundColor: '#55608f',
+              borderBottom: '1px solid silver',
+            }}
+          >
+            <th width="30" height="40px" style={{ textAlign: 'center' }}>
+              <p>번호</p>
+            </th>
+            <th width="50px">
+              <p>사진</p>
+            </th>
+            <th width="200">
+              <p>내용</p>
+            </th>
+            <th width="100">
+              <p>평점</p>
+            </th>
+            <th width="50">
+              <p>작성자</p>
+            </th>
+            <th width="80">
+              <p>작성일</p>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map(row => (
+              <tr
+                style={{
+                  height: '80px',
+                  borderTop: '1px solid black',
+                  backgroundColor: 'background-color: rgba(255, 255, 255, 0.3)',
+                }}
               >
-                삭제
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* class_reviewrow */}
+                <td style={{ textAlign: 'center' }}>
+                  <p>{row.classreview_num}</p>
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  <img
+                    alt=""
+                    className="photo_small"
+                    src={photoUrl + row.classreview_photo}
+                    style={{ margin: '5px' }}
+                  />
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  <p>{row.classreview_content}</p>
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  <p>
+                    {' '}
+                    <Rating
+                      name="size-medium"
+                      size="large"
+                      defaultValue={row.classreview_rate}
+                      readOnly
+                    />
+                  </p>
+                </td>
+
+                <td style={{ textAlign: 'center' }}>
+                  <p>{row.classreview_writer}</p>
+                </td>
+                <td style={{ textAlign: 'center' }}>
+                  <p>{row.classreview_date}</p>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
